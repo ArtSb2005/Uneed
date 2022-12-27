109e470d47063f20c9d41526c985a178d7e80764
ArtSb2005 <sboev.art@gmail.com>
2022-12-28 02:26:25 +0300

    commit

--- djangoUneed/settings.py
+++ djangoUneed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h=k=i#%_*lqq)t9t8igb-%!(3jm9zh42d%xe765ubw(&jcw6w1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
